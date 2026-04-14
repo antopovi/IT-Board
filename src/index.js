39c339c20f6de7ebd6bae5dd12860f283ee8218c
antopovi <antonio.povia@casillogroup.it>
2026-04-14 17:51:32 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ITBoard from "./ITBoard";
+import "./styles.css";
+
+ReactDOM.render(<ITBoard />, document.getElementById("root"));
